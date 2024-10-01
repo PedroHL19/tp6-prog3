c71f665264260960f7de71e8a4824fb8f88f4c87
PedroHL19 <pedrohlopes1903@gmail.com>
2024-10-01 13:08:17 -0300

    lista kenia completa

--- Main.java
+++ Main.java
@@ -7,17 +7,12 @@ class Main {
         arvore.Inserir(new No(5));
         arvore.Inserir(new No(3));
         arvore.Inserir(new No(11));
-        arvore.Inserir(new No(40));
-        //
-        //arvore.Inserir(new No(35));
-        arvore.printarArvore();
-        //arvore.encaminhamentosSemRecursividade();
-        //System.out.println("Número de nós: " + arvore.contarNos());
-        //System.out.println("Número de nós não-folha: " + arvore.contarNosNaoFolha());
-        //System.out.println("Número de folhas: " + arvore.contarFolhas());
-        //System.out.println("Altura da árvore: " + arvore.calculaAltura());
+        arvore.encaminhamentosSemRecursividade();
+        System.out.println("Número de nós: " + arvore.contarNos());
+        System.out.println("Número de nós não-folha: " + arvore.contarNosNaoFolha());
+        System.out.println("Número de folhas: " + arvore.contarFolhas());
+        System.out.println("Altura da árvore: " + arvore.calculaAltura());
         arvore.removerTodosPares();
-        //arvore.espelhaArvore();
-        arvore.printarArvore();
+        arvore.espelhaArvore();
     }
 }
--- TabalasHashNvdiaExemplos.java
+++ TabalasHashNvdiaExemplos.java
@@ -7,18 +7,14 @@ import java.util.concurrent.ConcurrentHashMap;
 public class TabalasHashNvdiaExemplos {
     public static void main(String[] args) {
         
-        HashMap<String, String> hashMapPlacas = new HashMap<>();
-        hashMapPlacas.put("RTX 3090", "24GB GDDR6X");
-        hashMapPlacas.put("RTX 3080", "10GB GDDR6X");
-        hashMapPlacas.put("RTX 3070", "8GB GDDR6");
-        hashMapPlacas.put("GTX 1660", "6GB GDDR5");
-        hashMapPlacas.put("RTX 3060 Ti", "8GB GDDR6");
-        System.out.println("Memória da placa RTX 3080: " + hashMapPlacas.get("RTX 3080"));
+        HashMap<String, String> hashMapPlacasNvdia = new HashMap<>();
+        hashMapPlacasNvdia.put("RTX 3090", "24GB GDDR6X");
+        hashMapPlacasNvdia.put("RTX 3080", "10GB GDDR6X");
+        hashMapPlacasNvdia.put("RTX 3060 Ti", "8GB GDDR6");
+        System.out.println("Memória da placa RTX 3080: " + hashMapPlacasNvdia.get("RTX 3080"));
         
 
         HashSet<String> hashSetModelos = new HashSet<>();
-        hashSetModelos.add("RTX 3090");
-        hashSetModelos.add("RTX 3080");
         hashSetModelos.add("RTX 3070");
         hashSetModelos.add("GTX 1660");
         hashSetModelos.add("RTX 3060 Ti");
@@ -29,16 +25,12 @@ public class TabalasHashNvdiaExemplos {
         hashtablePotencia.put("RTX 3090", 350);
         hashtablePotencia.put("RTX 3080", 290);
         hashtablePotencia.put("RTX 3070", 220);
-        hashtablePotencia.put("GTX 1660", 120);
-        hashtablePotencia.put("RTX 3060 Ti", 200);
         System.out.println("\nPotência de cálculo da placa RTX 3070: " + hashtablePotencia.get("RTX 3070") + " TFLOPS");
 
 
         LinkedHashMap<String, String> linkedHashMapClock = new LinkedHashMap<>();
         linkedHashMapClock.put("RTX 3090", "1.70 GHz");
         linkedHashMapClock.put("RTX 3080", "1.71 GHz");
-        linkedHashMapClock.put("RTX 3070", "1.50 GHz");
-        linkedHashMapClock.put("GTX 1660", "1.50 GHz");
         linkedHashMapClock.put("RTX 3060 Ti", "1.67 GHz");
         System.out.println("\nVelocidade de clock da RTX 3060 Ti: " + linkedHashMapClock.get("RTX 3060 Ti"));
 
@@ -46,8 +38,6 @@ public class TabalasHashNvdiaExemplos {
         concurrentHashMapVendas.put("RTX 3090", 1500);
         concurrentHashMapVendas.put("RTX 3080", 3000);
         concurrentHashMapVendas.put("RTX 3070", 2000);
-        concurrentHashMapVendas.put("GTX 1660", 5000);
-        concurrentHashMapVendas.put("RTX 3060 Ti", 2500);
         System.out.println("\nVendas da placa RTX 3080: " + concurrentHashMapVendas.get("RTX 3080") + " unidades");
     }
 }
